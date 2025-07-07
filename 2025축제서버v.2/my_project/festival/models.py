7728d24ae8143256b4e8fe7d29eaad01ae57c296
whiteblanket123 <yunsoo2007@gmail.com>
2025-07-07 11:09:13 +0900

    index, success_page

--- 2025축제서버v.2/my_project/festival/models.py
+++ 2025축제서버v.2/my_project/festival/models.py
@@ -10,6 +10,16 @@ class Information(models.Model):
         verbose_name = 'Information'  # 모델의 단수 이름
         verbose_name_plural = 'Informations'  # 모델의 복수 이름
 
+        # 인덱스 정의
+        indexes = [
+            # name과 number 컬럼에 대한 다중 컬럼 인덱스
+            # name: 인덱스 이름 (선택 사항, 없으면 장고가 자동 생성)
+            models.Index(fields=['name', 'number'], name='idx_information_name_number'),
+            
+            # vote 컬럼에 대한 단일 컬럼 인덱스
+            models.Index(fields=['vote'], name='idx_information_vote'),
+        ]
+
 class Participant(models.Model):
     participant1 = models.CharField(max_length=100)
     participant2 = models.CharField(max_length=100)
